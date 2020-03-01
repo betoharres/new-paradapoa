9d3fb4f893762c2107b006cd63369870170c6448
Alberto Rocha <betoharres@gmail.com>
2020-03-01 01:10:28 -0300

    Remove unused styles at Bus.styles.js

--- app/components/Bus/Bus.styles.js
+++ app/components/Bus/Bus.styles.js
@@ -1,24 +1,7 @@
-import {Dimensions} from 'react-native'
 import styled from 'styled-components/native'
-import {Card as RNECard, ListItem as RNEListItem} from 'react-native-elements'
-
-const {height: SCREEN_HEIGHT} = Dimensions.get('window')
 
 export const Container = styled.View`
   flex: 1;
   flex-direction: column;
   align-items: center;
 `
-
-export const Card = styled(RNECard).attrs(() => ({
-  containerStyle: {
-    borderRadius: 15,
-    maxHeight: SCREEN_HEIGHT - 205,
-    paddingBottom: 50,
-  },
-  dividerStyle: {
-    display: 'none',
-  },
-}))``
-
-export const ListItem = styled(RNEListItem).attrs(() => ({}))``
