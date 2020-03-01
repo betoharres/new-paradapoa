44797e1e54cbfaccf213188702dd0dc775de2b51
Alberto Rocha <betoharres@gmail.com>
2020-03-01 15:43:33 -0300

    docs(utils): add comments

--- app/components/Bus/lib/utils.js
+++ app/components/Bus/lib/utils.js
@@ -1,9 +1,9 @@
 export function getCurrentDayType(schedules) {
   const date = new Date()
   const day = date.getDay()
-  if (day >= 1 && day <= 5) return 0
-  else if (day === 6) return schedules.findIndex(i => i === '2')
-  else return schedules.length - 1
+  if (day >= 1 && day <= 5) return 0 // business days
+  else if (day === 6) return schedules.findIndex(i => i === '2') // saturday
+  else return schedules.length - 1 // sunday
 }
 
 export function getCurrentTimeDateTimeValue(dayType, hour, minute) {
