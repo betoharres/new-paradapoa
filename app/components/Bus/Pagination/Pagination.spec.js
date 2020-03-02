f50b15b40ae91aac5629ea5277eb87deac6ccc18
Alberto Rocha <betoharres@gmail.com>
2020-03-01 21:07:55 -0300

    test(Pagination): Add Pagination spec

---
+++ app/components/Bus/Pagination/Pagination.spec.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Pagination from './Pagination'
+import {render, mockProps} from '~/jest'
+import schedules from '~/jest/665.json'
+
+it('renders correctly', () => {
+  const props = mockProps({props: {entries: schedules, activeIndex: 0}})
+  const wrapper = render(<Pagination {...props} />)
+  expect(wrapper.toJSON()).toMatchSnapshot()
+})
